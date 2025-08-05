cf0960980a73e6902f69edcbd0957ced6ff50e56
Sergey Polivin <s.polivin@gmail.com>
2025-08-05 21:00:11 +0200

    feat(trainer): add new functionality to the Trainer

--- epoch_engine/core/trainer.py
+++ epoch_engine/core/trainer.py
@@ -3,7 +3,7 @@
 # Author: Sergey Polivin <s.polivin@gmail.com>
 # License: MIT License
 
-from typing import Any, TypeAlias
+from typing import Any, Callable, TypeAlias
 
 import torch
 from tqdm import tqdm
@@ -21,7 +21,6 @@ class Trainer:
         self,
         model: Any,
         criterion: Any,
-        optimizer: Any,
         train_loader: TorchDataloader,
         valid_loader: TorchDataloader,
         train_on: str = "auto",
@@ -31,10 +30,9 @@ class Trainer:
         Args:
             model (Any): PyTorch model instance.
             criterion (Any): PyTorch loss function instance.
-            optimizer (Any): PyTorch optimizer instance.
             train_loader (TorchDataloader): Torch Dataloader for training set.
             valid_loader (TorchDataloader): Torch Dataloader for validation set.
-            train_on (str, optional): Indicator of CPU- or GPU-based training. Defaults to "auto".
+            train_on (str, optional): Device to be used to train the model. Defaults to "auto".
 
         Attributes:
             model (Any): PyTorch model instance.
@@ -48,22 +46,38 @@ class Trainer:
             last_epoch (int): Last epoch when training has been successfully finished. Defaults to 0.
             metrics_tracker (MetricsTracker): Class for handling computing metrics.
             ckpt_handler (CheckpointHandler): Class for saving/loading checkpoints.
+
+        Raises:
+            TypeError: If model is not an instance of torch.nn.Module.
+            TypeError: If train_loader or valid_loader are not instances of torch.utils.data.DataLoader.
+            ValueError: If train_on is not one of "auto", "cpu", "cuda", or "mps".
         """
-        # Using the CPU or GPU device or inferring one
-        self.device = (
-            torch.device("cuda" if torch.cuda.is_available() else "cpu")
-            if train_on == "auto"
-            else torch.device(train_on)
-        )
-        # Moving model to the device specified
+        # Using the CUDA, MPS or CPU device or inferring one
+        if train_on == "auto":
+            device = self._auto_detect_device_type()
+        elif train_on in ("cpu", "cuda", "mps"):
+            device = train_on
+        else:
+            raise ValueError(
+                "Invalid device specified. Use 'auto', 'cpu', 'cuda', or 'mps'."
+            )
+        self.device = torch.device(device)
+
+        # Model
+        if not isinstance(model, torch.nn.Module):
+            raise TypeError("model must be an instance of torch.nn.Module")
         self.model = model.to(self.device)
-        # Reinitializing the optimizer with model parameters having been moved to device
-        self.optimizer = type(optimizer)(self.model.parameters(), **optimizer.defaults)
 
         # Setting loss function
         self.criterion = criterion
 
         # Setting dataloaders for training/validation sets
+        if not isinstance(
+            train_loader, torch.utils.data.DataLoader
+        ) or not isinstance(valid_loader, torch.utils.data.DataLoader):
+            raise TypeError(
+                "train_loader and valid_loader must be instances of torch.utils.data.DataLoader"
+            )
         self.train_loader = train_loader
         self.valid_loader = valid_loader
 
@@ -78,29 +92,62 @@ class Trainer:
         self.metrics_tracker = MetricsTracker()
         self.ckpt_handler = CheckpointHandler()
 
-    def set_scheduler(
+        self.extra_metrics = False
+
+    def _auto_detect_device_type(self) -> str:
+        """Automatically detects the device type to use for training.
+
+        Returns:
+            str: The detected device (CPU, CUDA, or MPS).
+        """
+        if torch.cuda.is_available():
+            return "cuda"
+        elif (
+            hasattr(torch.backends, "mps")
+            and torch.backends.mps.is_available()
+        ):
+            return "mps"
+        else:
+            return "cpu"
+
+    def configure_optimizers(
         self,
-        scheduler_class: Any,
-        scheduler_params: dict[str, Any],
-        level: str = "epoch",
+        optimizer_class: Any,
+        optimizer_params: dict[str, Any],
+        scheduler_class: Any = None,
+        scheduler_params: dict[str, Any] = None,
+        scheduler_level: str = "epoch",
     ) -> None:
-        """Sets and initializes a scheduler.
+        """Configures optimizer and (optionally) scheduler.
 
         Args:
-            scheduler_class (Any): Scheduler class from PyTorch
-            scheduler_params (dict[str, Any]): Scheduler parameters.
-            level (str, optional): Level on which to apply scheduler. Defaults to "epoch".
+            optimizer_class (Any): Optimizer class from PyTorch.
+            optimizer_params (dict[str, Any]): Optimizer parameters.
+            scheduler_class (Any, optional): Scheduler class from PyTorch. Defaults to None.
+            scheduler_params (dict[str, Any], optional): Scheduler parameters. Defaults to None.
+            scheduler_level (str, optional): Level for scheduler ("epoch" or "batch"). Defaults to "epoch".
 
         Raises:
-            ValueError: Error raised if the specified level name is unexpected.
+            TypeError: If optimizer_params or scheduler_params are not dictionaries.
+            ValueError: If scheduler_level is not "epoch" or "batch".
         """
-        # Setting scheduler level on batch- or epoch-level
-        if level not in ("epoch", "batch"):
-            raise ValueError("Invalid value")
-        else:
-            self.scheduler_level = level
-        # Initializing a scheduler
-        self.scheduler = scheduler_class(self.optimizer, **scheduler_params)
+        if not isinstance(optimizer_params, dict):
+            raise TypeError("optimizer_params must be a dictionary")
+        self.optimizer = optimizer_class(
+            self.model.parameters(), **optimizer_params
+        )
+        if scheduler_class is not None and scheduler_params is not None:
+            if not isinstance(scheduler_params, dict):
+                raise TypeError("scheduler_params must be a dictionary")
+            if scheduler_level not in ("epoch", "batch"):
+                raise ValueError("Invalid scheduler_level")
+            self.scheduler_level = scheduler_level
+            self.scheduler = scheduler_class(
+                self.optimizer, **scheduler_params
+            )
+            print(
+                f"Scheduler has been configured with level: {scheduler_level}"
+            )
 
     def reset_scheduler(self) -> None:
         """Resetting and removing scheduler."""
@@ -137,9 +184,16 @@ class Trainer:
             seed (int, optional): Random number generator seed. Defaults to 42.
             enable_tqdm (bool, optional): Indicator to turn on tqdm progress bar. Defaults to True.
         """
+        if not hasattr(self, "optimizer"):
+            raise ValueError(
+                "Optimizer must be configured before running the trainer. Use `configure_optimizers` method."
+            )
         # Setting seed
         if seed:
-            torch.manual_seed(seed)
+            if torch.cuda.is_available():
+                torch.cuda.manual_seed(seed)
+            else:
+                torch.manual_seed(seed)
         # Computing the number of total epochs to be trained (useful in case of additional trainings)
         total_epochs = epochs + self.last_epoch
         # Running training/validation loops
@@ -157,7 +211,9 @@ class Trainer:
             # Resetting metrics counters after successful training/validation
             self.metrics_tracker.reset()
 
-    def _train_one_epoch(self, epoch: int, epochs: int, enable_tqdm: bool) -> None:
+    def _train_one_epoch(
+        self, epoch: int, epochs: int, enable_tqdm: bool
+    ) -> None:
         """Processes all training batches for one epoch.
 
         Args:
@@ -186,7 +242,9 @@ class Trainer:
         if self.scheduler and self.scheduler_level == "epoch":
             self.scheduler.step()
 
-    def _validate_one_epoch(self, epoch: int, epochs: int, enable_tqdm: bool) -> None:
+    def _validate_one_epoch(
+        self, epoch: int, epochs: int, enable_tqdm: bool
+    ) -> None:
         """Processes all validation batches for one epoch.
 
         Args:
@@ -202,7 +260,7 @@ class Trainer:
             unit="batches",
             disable=not enable_tqdm,
         ) as pbar:
-            # Setting model to evalutation model
+            # Setting model to evaluation model
             self.model.eval()
             # Processing all validation batches (grad compute turned off)
             with torch.no_grad():
@@ -211,20 +269,21 @@ class Trainer:
                     pbar.update(1)
 
             # Aggregating batch-level metrics (loss, accuracy) to epoch-level
-            self.metrics = self.metrics_tracker.get_all_metrics()
+            self.metrics = self.metrics_tracker.compute_metrics()
             # Setting the epoch-level stats to the progress bar
-            pbar.set_postfix(
-                {
-                    "loss": (
-                        round(self.metrics["loss/train"], 4),
-                        round(self.metrics["loss/valid"], 4),
-                    ),
-                    "acc": (
-                        round(self.metrics["accuracy/train"], 4),
-                        round(self.metrics["accuracy/valid"], 4),
-                    ),
-                }
-            )
+            epoch_stats = {
+                "loss": (
+                    round(self.metrics["loss/train"], 4),
+                    round(self.metrics["loss/valid"], 4),
+                )
+            }
+            if self.extra_metrics:
+                for metric in self.metrics_tracker.metric_fns.keys():
+                    epoch_stats[metric] = (
+                        round(self.metrics[f"{metric}/train"], 4),
+                        round(self.metrics[f"{metric}/valid"], 4),
+                    )
+            pbar.set_postfix(epoch_stats)
 
     def _process_batch(self, batch: list[torch.Tensor], split: str) -> None:
         """Backpropagates model during training and collects batch-level metrics.
@@ -250,7 +309,9 @@ class Trainer:
                 self.scheduler.step()
 
         # Collecting batch-level metrics
-        self._collect_metrics(loss=loss, outputs=outputs, labels=y_batch, split=split)
+        self._collect_metrics(
+            loss=loss, outputs=outputs, labels=y_batch, split=split
+        )
 
     def _collect_metrics(
         self,
@@ -272,10 +333,23 @@ class Trainer:
         self.metrics_tracker.update(f"loss/{split}", loss)
 
         # Adding batch-level accuracy
-        predictions = torch.argmax(outputs, dim=1)
-        correct = (predictions == labels).sum().cpu().item()
-        total = labels.size(0)
-        self.metrics_tracker.update_accuracy(correct, total, split=split)
+        if self.extra_metrics:
+            predictions = torch.argmax(outputs, dim=1)
+            self.metrics_tracker.update_preds(
+                split=split,
+                preds=predictions.cpu().numpy(),
+                targets=labels.cpu().numpy(),
+            )
+
+    def register_metric(self, name: str, metric_fn: Callable) -> None:
+        """Registers a custom metric function.
+
+        Args:
+            name (str): Name of the metric.
+            metric_fn (Callable): Function to compute the metric.
+        """
+        self.metrics_tracker.register_metric(name=name, fn=metric_fn)
+        self.extra_metrics = True
 
     def save_checkpoint(self, path: str) -> None:
         """Saves trainer checkpoint.
