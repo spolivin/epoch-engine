283a82bf3c40a6e03c6ac6be18e8558b51507e57
Sergey Polivin <s.polivin@gmail.com>
2025-08-05 20:52:49 +0200

    feat(metrics-tracker): support for flexible metric computation

--- epoch_engine/core/metrics_tracker.py
+++ epoch_engine/core/metrics_tracker.py
@@ -4,97 +4,53 @@
 # License: MIT License
 
 
+from typing import Any, Callable
+
+
 class MetricsTracker:
-    """Class for keeping track of metrics."""
-
     def __init__(self) -> None:
-        """Initializes a class instance.
+        self.metrics: dict[str, list[float]] = {}
+        self.metric_fns: dict[str, Callable] = {}
+        self.preds: dict[str, list[Any]] = {}
+        self.targets: dict[str, list[Any]] = {}
 
-        Attributes:
-            metrics (dict[str, list[float]]): Batch-level metrics in a dict format.
-            total_correct_train (int): Number of correct predictions in train set.
-            total_samples_train (int): Total number of examples in train set.
-            total_correct_valid (int): Number of correct predictions in valid set.
-            total_samples_valid (int): Total number of examples in valid set.
-        """
-        self.metrics = {}
-        self.total_correct_train = 0
-        self.total_samples_train = 0
-        self.total_correct_valid = 0
-        self.total_samples_valid = 0
-
-    def reset(self) -> None:
-        """Resets all metrics and counters."""
-        self.metrics = {}
-        self.total_correct_train = 0
-        self.total_samples_train = 0
-        self.total_correct_valid = 0
-        self.total_samples_valid = 0
+    def register_metric(self, name: str, fn: Callable) -> None:
+        """Register a metric function. fn(y_true, y_pred) -> float"""
+        self.metric_fns[name] = fn
 
     def update(self, name: str, value: float) -> None:
-        """Updates a specific metric.
-
-        Args:
-            name (str): Metric name.
-            value (float): Value of a metric.
-        """
         if name not in self.metrics:
             self.metrics[name] = []
         self.metrics[name].append(value)
 
-    def update_accuracy(self, correct: int, total: int, split: str) -> None:
-        """Updates accuracy counters.
+    def update_preds(self, split: str, preds, targets):
+        if split not in self.preds:
+            self.preds[split] = []
+            self.targets[split] = []
+        self.preds[split].extend(preds)
+        self.targets[split].extend(targets)
 
-        Args:
-            correct (int): Number of correct predictions.
-            total (int): Number of total examples.
-            split (str): Train or valid split indicator.
-
-        Raises:
-            ValueError: Error in case some other split name is passed.
-        """
-        if split == "train":
-            self.total_correct_train += correct
-            self.total_samples_train += total
-        elif split == "valid":
-            self.total_correct_valid += correct
-            self.total_samples_valid += total
-        else:
-            raise ValueError
-
-    def compute_accuracy(self, split: str) -> float:
-        """Computes accuracy across all batches.
-
-        Args:
-            split (str): Train or valid split indicator.
-
-        Raises:
-            ValueError: Error in case some other split name is passed.
+    def compute_metrics(self) -> dict[str, float]:
+        """Compute the metrics.
 
         Returns:
-            float: Accuracy score.
+            Dict[str, float]: A dictionary containing the computed metrics.
         """
-        if self.total_samples_train == 0 or self.total_samples_valid == 0:
-            return 0.0
-        if split == "train":
-            return self.total_correct_train / self.total_samples_train
-        elif split == "valid":
-            return self.total_correct_valid / self.total_samples_valid
-        else:
-            raise ValueError
+        results = {}
+        # Average batch metrics
+        for name, values in self.metrics.items():
+            results[name] = sum(values) / len(values)
+        # Registered metrics
+        if self.metric_fns is not {}:
+            for name, fn in self.metric_fns.items():
+                for split in ["train", "valid"]:
+                    results[name + f"/{split}"] = fn(
+                        self.targets[split], self.preds[split]
+                    )
+        return results
 
-    def get_all_metrics(self) -> dict[str, float]:
-        """Computes all metrics as averages.
-
-        Returns:
-            dict[str, float]: Epoch-level metrics.
-        """
-        # Aggregating loss across batches
-        metrics = {
-            name: sum(values) / len(values) for name, values in self.metrics.items()
-        }
-        # Computing accuracy scores
-        metrics["accuracy/train"] = self.compute_accuracy(split="train")
-        metrics["accuracy/valid"] = self.compute_accuracy(split="valid")
-
-        return metrics
+    def reset(self) -> None:
+        """Reset the metrics tracker."""
+        self.metrics = {}
+        self.preds = {}
+        self.targets = {}
