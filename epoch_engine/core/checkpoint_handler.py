2c02560e82a6e9a48e209ba3a9c6730734b5114b
Sergey Polivin <s.polivin@gmail.com>
2025-08-05 20:56:06 +0200

    fix(checkpoint-handler): raise error in case optimizer has not been properly configured in Trainer

--- epoch_engine/core/checkpoint_handler.py
+++ epoch_engine/core/checkpoint_handler.py
@@ -15,7 +15,14 @@ class CheckpointHandler:
         Args:
             trainer: Trainer instance.
             path (str): Path of the checkpoint file to which data are to be saved.
+
+        Raises:
+            ValueError: If optimizer is not configured before saving a checkpoint.
         """
+        if not hasattr(trainer, "optimizer"):
+            raise ValueError(
+                "Optimizer must be configured before saving a checkpoint. Use `configure_optimizers` method."
+            )
         torch.save(
             {
                 "epoch": trainer.last_epoch,
@@ -31,7 +38,14 @@ class CheckpointHandler:
         Args:
             trainer: Trainer instance.
             path (str): Path of the checkpoint file from which data are to be loaded.
+
+        Raises:
+            ValueError: If optimizer is not configured before loading a checkpoint.
         """
+        if not hasattr(trainer, "optimizer"):
+            raise ValueError(
+                "Optimizer must be configured before loading a checkpoint. Use `configure_optimizers` method."
+            )
         # Loading the checkpoint file
         checkpoint = torch.load(path, weights_only=True)
         # Loading the data into the Trainer instance attributes
