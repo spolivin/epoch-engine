5824114a448cacfdca189dfd7912dd5769ad33ab
Sergey Polivin <s.polivin@gmail.com>
2025-08-12 12:12:57 +0200

    refactor(package): expose modules for shorter imports statements

---
+++ epoch_engine/core/__init__.py
@@ -0,0 +1,7 @@
+# Author: Sergey Polivin <s.polivin@gmail.com>
+# License: MIT License
+
+# ruff: noqa: F401
+
+from .configs import OptimizerConfig, SchedulerConfig
+from .trainer import Trainer
---
+++ epoch_engine/models/__init__.py
@@ -0,0 +1,6 @@
+# Author: Sergey Polivin <s.polivin@gmail.com>
+# License: MIT License
+
+# ruff: noqa: F401
+
+from .architectures import BasicBlock, EDNet, ResNet
